bfbe60818e62fcf11ea6ed84eab5b3875202f56a
Lorena Rios <lorenadejesusrios@gmail.com>
2025-07-11 14:25:37 -0300

    fix: Ajustada a Navbar no moblie

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -78,3 +78,15 @@ const videos = document.querySelectorAll("video");
     });
 
 
+  // MENU HAMBURGUER
+  document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('menu-btn');
+    const menu = document.getElementById('menu');
+
+    btn.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+    });
+  });
+
+
+
